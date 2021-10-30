00a14a346dc644e8f83a3323e2b05da32044832e
MrizalMuhaimin <muhammadrizalm0109@gmail.com>
2021-10-30 17:13:16 +0700

    liverelod delate

--- companyProfileKPMBB/settings.py
+++ companyProfileKPMBB/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'livereload',
     'django.contrib.staticfiles',
     'login',
 ]
@@ -52,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
+    
 ]
 
 ROOT_URLCONF = 'companyProfileKPMBB.urls'
